3ed57694f1e0492af1e29f527df276beb12d567e
Pepeuch <pepeuch@gmail.com>
2025-03-02 13:26:22 +0100

    modified:   package.json 	modified:   web/package.json 	modified:   web/src/components/DrawControl.tsx 	modified:   web/src/pages/MapPage.tsx 	modified:   web/vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        react(),
-    ],
+    plugins: [react()],
     resolve: {
         alias: {
             "react-map-gl": "@vis.gl/react-mapbox", // Corrige l'import de Mapbox
@@ -18,5 +16,20 @@ export default defineConfig({
                 ws: true,
             }
         }
-    }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('react')) return 'react';   // Séparer React
+                        if (id.includes('antd')) return 'antd';     // Séparer Ant Design
+                        if (id.includes('mapbox-gl')) return 'mapbox'; // Séparer Mapbox
+                        return 'vendor'; // Met le reste des dépendances dans un fichier séparé
+                    }
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Évite les avertissements inutiles
+    },
 });
